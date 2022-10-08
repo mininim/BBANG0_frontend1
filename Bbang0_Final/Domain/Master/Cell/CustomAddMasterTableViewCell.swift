9b4f9fbf9fd5bb726550cd945fb92bedd617ba63
Sarang <vvtkfkddl11@naver.com>
2022-10-08 15:47:25 +0900

    리아 파일 추가

---
+++ Bbang0_Final/Domain/Master/Cell/CustomAddMasterTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomAddMasterTableViewCell.swift
+//  Bbang0_Final
+//
+//  Created by 김사랑 on 2022/10/08.
+//
+
+import UIKit
+
+class CustomAddMasterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
