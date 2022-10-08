1883ddcb8e95fd133da65fd6904700d6a1b51534
minsop <minsoplee2015@gmail.com>
2022-10-08 16:55:50 +0900

    test git

---
+++ Bbang0_Final/Domain/Map/testcccc.swift
@@ -0,0 +1,22 @@
+//
+//  testcccc.swift
+//  Bbang0_Final
+//
+//  Created by 이민섭 on 2022/10/08.
+//
+
+import UIKit
+
+//for git testing 
+
+class testcccc: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
