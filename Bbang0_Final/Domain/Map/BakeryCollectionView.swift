16f31a172fb3bbf8f4b4a598fe50c114586a793d
minsop <minsoplee2015@gmail.com>
2022-10-08 17:19:35 +0900

    map main 화면 빈 파일 추가

---
+++ Bbang0_Final/Domain/Map/BakeryCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  BakeryCollectionView.swift
+//  Bbang0_Final
+//
+//  Created by 이민섭 on 2022/10/08.
+//
+
+import UIKit
+
+class BakeryCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Bbang0_Final/Domain/Map/BakeryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BakeryCollectionViewCell.swift
+//  Bbang0_Final
+//
+//  Created by 이민섭 on 2022/10/08.
+//
+
+import UIKit
+
+class BakeryCollectionViewCell: UICollectionViewCell {
+    
+}
