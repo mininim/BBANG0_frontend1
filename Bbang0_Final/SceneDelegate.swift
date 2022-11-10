e47e174c3d1d5b28912175f37a68d2ed7e28c2bb
Sarang <vvtkfkddl11@naver.com>
2022-11-10 21:32:04 +0900

    리아 네비게이션 추가 중 11/10

--- Bbang0_Final/SceneDelegate.swift
+++ Bbang0_Final/SceneDelegate.swift
@@ -17,6 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene: windowScene) // SceneDelegate의 프로퍼티에 설정해줌
+//        let rootVC = ViewController()
+//        let navigationController = UINavigationController(rootViewController: rootVC)
+//        window?.rootViewController = navigationController
+//        window?.makeKeyAndVisible()
+ 
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
